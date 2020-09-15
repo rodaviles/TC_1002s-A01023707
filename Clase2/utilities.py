5debc422e0304e119c05412fd7279fb7ab0c1587
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-15 11:02:29 -0500

    Clase 2

--- Clase2/utilities.py
+++ Clase2/utilities.py
@@ -7,5 +7,8 @@ class Student():
     def add_grades(self,grades):
         self.grades.append(grade)
         print("A grade of {} was added to de student {} {}".format(grade,self.name,self.last_name))
-edo=Student("Edoardo","Buchelli","MCC")
-print(edo.major)
+rod= Student("Rodrigo","Aviles","IQ")
+print(rod.major)
+print(rod.grades)
+rod add_grades (95)
+print (rod.grades)
