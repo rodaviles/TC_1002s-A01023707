e71bfcec4de08b91b46698e976b18cd79300895c
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-15 13:21:37 -0500

    Clase 2

--- Clase2/io_utilities.py
+++ Clase2/io_utilities.py
@@ -1,17 +1,18 @@
 #io_utilities.py
+filepath= './data/iris.data'
 
-filepath = './data/iris.data'
+def read_vanilla(filepath):
+    with open(filepath,'r') as fp:
+        data=fp.read()
 
-with open(filepath,'r') as fp:
-    data=fp.read()
-
-data_lines = data.split('\n')
-data_final= []
+    data_lines = data.split('\n')
+    data_final= []
 
 
-for line in data_lines:
-    data_final.append(line.split(','))
+    for line in data_lines:
+        data_final.append(line.split(','))
 
-data_final= [f,split(',') for f in data_lines]
+    data_final= [f.split(',') for f in data_lines]
+    return data_final
 
-print(data_final)
+print(read_vanilla(filepath))
