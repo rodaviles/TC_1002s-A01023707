509be524e5eed88e060d1eec7824ffea73bf571e
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-15 13:19:43 -0500

    Lector de Texto (Vanilla)

---
+++ io_utilities.py
@@ -0,0 +1,16 @@
+#io_utilities.py
+filepath= './data/iris.data'
+
+with open(filepath,'r') as fp:
+    data=fp.read()
+
+data_lines = data.split('\n')
+data_final= []
+
+
+for line in data_lines:
+    data_final.append(line.split(','))
+
+data_final= [f.split(',') for f in data_lines]
+
+print(data_final)
