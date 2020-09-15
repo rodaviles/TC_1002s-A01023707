6fd70b8dd68d6f0f4fe5e6d74acf2eedd69dde9d
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-15 11:36:06 -0500

    Clase 2

--- Clase2/io_utilities.py
+++ Clase2/io_utilities.py
@@ -1,6 +1,17 @@
 #io_utilities.py
 
-fp= open('./Clase2/Clase2.dta','r')
-data= fp.read()
-print(data)
-fp.close()
+filepath = './data/iris.data'
+
+with open(filepath,'r') as fp:
+    data=fp.read()
+
+data_lines = data.split('\n')
+data_final= []
+
+
+for line in data_lines:
+    data_final.append(line.split(','))
+
+data_final= [f,split(',') for f in data_lines]
+
+print(data_final)
