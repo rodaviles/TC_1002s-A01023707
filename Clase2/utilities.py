32d833c6360a36483cd936e865b531034f0efc41
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-15 11:08:19 -0500

    Clase 2

--- Clase2/utilities.py
+++ Clase2/utilities.py
@@ -1,3 +1,4 @@
+import numpy as np
 class Student():
     def _init_(self,name,last_name,major):
         self.name= name
@@ -7,7 +8,12 @@ class Student():
     def add_grades(self,grades):
         self.grades.append(grade)
         print("A grade of {} was added to de student {} {}".format(grade,self.name,self.last_name))
-rod= Student("Rodrigo","Aviles","IQ")
+    def get_average(self):
+        return np.mean(self.grades)
+
+
+
+rod= Student("Rodrigo","Beceiro","IQ")
 print(rod.major)
 print(rod.grades)
 rod add_grades (95)
