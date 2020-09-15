893501b55640493764cdb2cfef13ac8627f8194e
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-15 13:39:37 -0500

    Lector de Texto (tablas)

---
+++ utilities_pandas.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def read_pandas(filepath, names):
+    df= pd.read.csv(
+    filepath,
+    names=names,
+    )
+    return df
+
+
+if _name_=="_main_":
+    filepath= "./data/iris.data"
+    names= ['sepal_length','sepal_width','petal_length','pertal_width','class']
+    df=read_pandas(filepath, names)
+    print(df.head(10))
+
+    sepal_length = df['sepal_length'].to_list()
+    print(sepal_length)
+
+
+    df['sl+pl']=df['sepal_length']+df['petal_length']
+
+    print(df.head())
+
+    df_filtred=df[df['sepal_length']>=5]
+
+    print (df_filtred)
