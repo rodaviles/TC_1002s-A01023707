6e925238998df71fae47db06d4e57446fcd5ce12
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-16 11:26:15 -0500

    Tablas (Pandas)

--- utilities_pandas.py
+++ utilities_pandas.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
 def read_pandas(filepath, names):
-    df= pd.read.csv(
-    filepath,
-    names=names,
+    df= pd.read_csv(
+        filepath,
+        names=names,
     )
+
     return df
 
 
-if _name_=="_main_":
+if __name__ == "__main__":
     filepath= "./data/iris.data"
     names= ['sepal_length','sepal_width','petal_length','pertal_width','class']
-    df=read_pandas(filepath, names)
+    df= read_pandas(filepath, names)
     print(df.head(10))
 
     sepal_length = df['sepal_length'].to_list()
