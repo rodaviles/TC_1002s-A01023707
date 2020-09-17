13c24f3bab0b6a2b08ecba9db565f784124126d0
rodaviles63@gmail.com <rodaviles@Rods-MacBook-Pro.local>
2020-09-17 14:36:58 -0500

    Clase 3

--- Clase3/kmeans.py
+++ Clase3/kmeans.py
@@ -2,21 +2,21 @@
 
 import numpy as np
 
-def distance(a,b):
+def distancia(x,y):
 
-    a=np.array(a)**2
-    b=np.array(b)**2
-    return np.sqrt(a+b)
+    x=np.array(x)**2
+    y=np.array(y)**2
+    return np.sqrt(x+y)
 
 def close (puntos,centros):
     closest=[]
     clusters=[ [] for f in centros]
 
     for i, punto in enumerate(puntos):
-        this_distance=[]
+        esta_distancia=[]
         for j,centro in enumerate(centros):
-            this_distance.append(distance(punto,centro))
-        this_argmin=np.arg_min(this_distance)
+            esta_distancia.append(distancia(punto,centro))
+        this_argmin=np.argmin(esta_distancia)
         closest.append(this_argmin)
         clusters[this_argmin].append(punto)
 
@@ -28,3 +28,23 @@ def centers(clusters):
         sum=0
         for i in range(len(cluster)):
             sum+= cluster[i]
+list_x = []
+list_y = []
+num = int(input("Please input the size of the lists \n"))
+c1=0
+
+while c1<num:
+    x= int(input("Input x value \n"))
+    y= int(input("Input y value \n"))
+    list_x.append(x)
+    list_y.append(y)
+    c1=c1+1
+
+
+print(list_x)
+print(list_y)
+
+print("The distance is {}".format(distancia(list_x,list_y)))
+print("They are {} close".format(close(list_x,list_y)))
+print("The centers in x are {}".format(centers(list_x)))
+print("The centers in y are {}".format(centers(list_y)))
